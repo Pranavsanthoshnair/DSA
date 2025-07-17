efb51084a32f3ef327deebfe4e78ea53fb3ebd04
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-07-17 12:18:21 +0530

    Update sumofp.c

--- sumofp.c
+++ sumofp.c
@@ -5,36 +5,36 @@ struct Term {
     int expo;
 };
 
-void addPolynomials(struct Term poly1[], int n1, struct Term poly2[], int n2, struct Term polyResult[], int *nResult) {
+void addPolynomials(struct Term poly1[], int n1, struct Term poly2[], int n2, struct Term polyResult[], int *nResult) {        // *nResult - used to store the count of the resulting terms
     int i = 0, j = 0, k = 0;
 
-    while (i < n1 && j < n2) {
-        if (poly1[i].expo == poly2[j].expo) {
+    while (i < n1 && j < n2) {                                                                                                 // Comparing first polynomial with second polynomial.
+        if (poly1[i].expo == poly2[j].expo) {                        // Checking equal or not first.
             polyResult[k].coeff = poly1[i].coeff + poly2[j].coeff;
             polyResult[k].expo = poly1[i].expo;
             i++;
             j++;
             k++;
         }
-        else if (poly1[i].expo > poly2[j].expo) {
+        else if (poly1[i].expo > poly2[j].expo) {                                                                              // if term of 1st poly is greater print it.
             polyResult[k] = poly1[i];
             i++;
             k++;
         }
-        else {
+        else {                                                                                                                 // if term of 2nd poly is greater print it.
             polyResult[k] = poly2[j];
             j++;
             k++;
         }
     }
 
-    while (i < n1) {
+    while (i < n1) {                                                                                                           // Comparing or looking for 1st poly terms when second has none to compare anymore.
         polyResult[k] = poly1[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2) {                                                                                                           // Comparing or looking for 2nd poly terms when first has none to compare anymore.
         polyResult[k] = poly2[j];
         j++;
         k++;
@@ -42,8 +42,8 @@ void addPolynomials(struct Term poly1[], int n1, struct Term poly2[], int n2, st
 
     *nResult = k; 
 }
-
-void displayPolynomial(struct Term poly[], int n) {
+ 
+void displayPolynomial(struct Term poly[], int n) {                                                                            // To Display Polynomial.
     for (int i = 0; i < n; i++) {
         printf("%dx^%d", poly[i].coeff, poly[i].expo);
         if (i != n - 1) {
@@ -53,11 +53,11 @@ void displayPolynomial(struct Term poly[], int n) {
     printf("\n");
 }
 
-void readPolynomial(struct Term poly[], int *n) {
+void readPolynomial(struct Term poly[], int *n) {                                                                              // To Read Polynomials
     printf("Enter the number of terms in the polynomial: ");
     scanf("%d", n);
 
-    for (int i = 0; i < *n; i++) {
+    for (int i = 0; i < *n; i++) {used to store the count of the resulting terms
         printf("Enter coefficient and exponent for term %d: ", i + 1);
         scanf("%d %d", &poly[i].coeff, &poly[i].expo);
     }
