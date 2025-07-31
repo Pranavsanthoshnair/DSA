2e695305d1ebba34f1d0d41820eff3604f5d8390
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-07-31 11:05:53 +0530

    Update Queue(OP).c

--- Queue(OP).c
+++ Queue(OP).c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #define MAX 5
 
-int queue[MAX];
-int front = -1, rear = -1;
+int queue[MAX] = {11, 22, 33};
+int front = 0;
+int rear = 2;
 
-// Function to insert element into queue
 void enqueue(int value) {
     if (rear == MAX - 1) {
         printf("Queue is full (Overflow)\n");
+    } else if (rear == -1) {
+        front = 0;
+        rear = 0;#include <stdio.h>
+#define MAX 5
+
+int queue[MAX] = {11, 22, 33};
+int front = 0;
+int rear = 2;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue is full (Overflow)\n");
+    } else if (rear == -1) {
+        front = 0;
+        rear = 0;
+        queue[rear] = value;
+        printf("%d inserted into the queue\n", value);
     } else {
-        if (front == -1) front = 0; // First insertion
-        rear++;
+        rear = rear + 1;
         queue[rear] = value;
         printf("%d inserted into the queue\n", value);
     }
 }
 
-// Function to delete element from queue
 void dequeue() {
-    if (front == -1 || front > rear) {
+    if (front == -1) {
         printf("Queue is empty (Underflow)\n");
+    } else if (front == rear) {
+        printf("Deleted: %d\n", queue[front]);
+        front = -1;
+        rear = -1;
     } else {
         printf("Deleted: %d\n", queue[front]);
         front++;
-        if (front > rear) {
-            // Reset queue when all elements are removed
-            front = rear = -1;
-        }
     }
 }
 
-// Function to display queue elements
 void display() {
     if (front == -1) {
         printf("Queue is empty\n");
@@ -43,19 +57,91 @@ void display() {
     }
 }
 
-// Main function
 int main() {
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
+    front = -1; rear = -1;
+    
+    enqueue(11);
+    enqueue(22);
+    enqueue(33);
     display();
 
     dequeue();
     display();
 
-    enqueue(40);
-    enqueue(50);
-    enqueue(60);  // This will show overflow
+    enqueue(44);
+    enqueue(55);
+    enqueue(66);
+    display();
+
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+
+    enqueue(77);
+    display();
+
+    return 0;
+}
+
+        queue[rear] = value;
+        printf("%d inserted into the queue\n", value);
+    } else {
+        rear = rear + 1;
+        queue[rear] = value;
+        printf("%d inserted into the queue\n", value);
+    }
+}
+
+void dequeue() {
+    if (front == -1) {
+        printf("Queue is empty (Underflow)\n");
+    } else if (front == rear) {
+        printf("Deleted: %d\n", queue[front]);
+        front = -1;
+        rear = -1;
+    } else {
+        printf("Deleted: %d\n", queue[front]);
+        front++;
+    }
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue is empty\n");
+    } else {
+        printf("Queue: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    front = -1; rear = -1;
+    
+    enqueue(11);
+    enqueue(22);
+    enqueue(33);
+    display();
+
+    dequeue();
+    display();
+
+    enqueue(44);
+    enqueue(55);
+    enqueue(66);
+    display();
+
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+
+    enqueue(77);
     display();
 
     return 0;
